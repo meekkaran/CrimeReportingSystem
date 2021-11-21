d4f36b308705082962c2ccf12850db2100aaea11
Meek Karan <meekkaran@gmail.com>
2021-11-21 23:50:25 +0300

    making the navbar responsive

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   <Router>
     <Navbar />
       <Routes>
-          <Route path="/home" element={ <Home />} />
+          <Route path="/" element={ <Home />} />
       </Routes>
       <Routes>
           <Route path="/about" element={<About/>} />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import "./Navbar.css"
 
 const Navbar = () => {
+
+    const [isMobile, setIsMobile] = useState(false);
     return (
         <nav className="navbar">
             <h3 className="logo">Logo</h3>
-            <ul className="nav-links">
+            <ul className={isMobile ? "nav-links-mobile":"nav-links"}
+            onClick={() => setIsMobile(false)}>
                 <Link to = "/" className="home">
                     <li>Home</li>
                 </Link>
@@ -23,6 +26,14 @@ const Navbar = () => {
                     <li>Support</li>
                 </Link>
             </ul>
+            <button className="mobile-menu-icon"
+            onClick={()=> setIsMobile(!isMobile)}>
+                {isMobile ? (
+                    <i className="fas fa-times"></i>
+                ) : (
+                    <i className="fas fa-bars"></i>
+                )}
+            </button>
         </nav>
     )
 }
