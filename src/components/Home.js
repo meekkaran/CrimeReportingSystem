abb6208c38a3984066da344b0fdfc9c640c55b81
Meek Karan <meekkaran@gmail.com>
2021-12-01 17:20:53 +0300

    working with css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import "./home.css"
 
+
 const Home = () => {
     return (
         <div className="main">
-            home
-            {/* <img className="image" src="../img/crimescene.jpg" /> */}
-            {/* <div className="bodycontent">
-                <h1>Reporting crimes hapening in-time and getting help is really hard</h1>
-                <h2>This system allows one to report crimes online from wherever they are and they could get immediate help</h2>
-                <div className="butttons">
-                    <button className="button">Report Crime</button>
-                    <button className="button">Suppor</button>
-                </div>
-            </div> */}
+            <div className="bodycontent">
+                <div className="row">
+                    <div className="col-lg-6 col-md-6">
+                        <div className="content">
+                            <h1>Reporting crimes hapening in-time and getting help is really hard</h1>
+                            <h3>This system allows one to report crimes online from wherever they are and they could get immediate help</h3>
+                            <h2>Save your life.Save Humanity</h2>
+                            <div className="buttons">
+                                <button className="button1">Report Crime</button>
+                                <button className="button2">Support</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
         </div>
     )
-}
+};
 
 export default Home
