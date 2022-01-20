8d6eba32be5c106aacd23c60d81cb6629ca5dcbc
Meek Karan <meekkaran@gmail.com>
2022-01-20 09:57:31 +0300

    footer section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css"
 import Navbar from "./components/Navbar/Navbar";
+import {FooterContainer} from './containers/footer'
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <FooterContainer />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {GlobalStyles} from "./global-styles"
 
 ReactDOM.render(
-    <App />,
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
   document.getElementById('root')
 );
 
