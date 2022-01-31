895dc1abb914852d587d601e4f4da102cfda19d1
Meek Karan <meekkaran@gmail.com>
2022-01-31 21:43:28 +0300

    body content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import {Container,Row,Col} from "react-bootstrap";
 
 function Contact() {
-  return <div>contact us</div>;
-}
+  return(
+    <div>
+      <Container>
+        <Row className='mb-5 mt-3'>
+          <Col lg-8>
+            <h1 className='display-4 mb-4'>
+              Contact Us
+            </h1>
+          </Col>
+        </Row>
 
+        <Row className='sec_sp'>
+          <Col lg="5" className='mb-5'>
+            <h3 className='color_sec py-4'>Get in Touch</h3>
+            <address>
+              <strong>Email: meekkaran@gmail.com</strong>
+              <br/>
+              <br/>
+              <p>
+                <strong>Phone : +254 713 201 600</strong>
+              </p>
+            </address>
+            <p>In this approach. Printed questionnaires were given to some of the students, lecturers 
+              and coordinators to fill in the blank spaces.  </p>
+          </Col>
+          <Col lg='/' className="d-flex align-ites-center">
+            <form className='="contact_form w-100'>
+              <Row>
+                <Col lg="6" className='form-group'>
+                  <input 
+                    className='="form-control'
+                    id="name"
+                    name="name"
+                    placeholder='Name'
+                    type="text"
+                    />
+                </Col>
+                <Col lg="6" className='form-group'>
+                  <input 
+                    className='="form-control rounded-o'
+                    id="email"
+                    name="email"
+                    placeholder='Email'
+                    type="email"
+                    />
+                </Col>
+              </Row>
+              <textarea className='="form-control rounded-o'
+                        id="message"
+                        placeholder="Message"
+                        rows='5'
+              ></textarea>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+
+  ) 
+  }
 export default Contact;
